74a8b1c96d31ffdee79a601559cbc4999bf31cb6
hell03610 <eraseunavez@gmail.com>
2014-08-09 20:00:50 +0100

    Add Watch task to files

--- client/gruntfile.js
+++ client/gruntfile.js
@@ -27,16 +27,25 @@ module.exports = function(grunt) {
       public: {
         files:[{expand: true, src: ['public/**'], dest: '../server/'}],
       }
+    },
+    watch: {
+      dist: {
+        files: ['src/**/*.js','public/index.html','public/css/**'],
+        tasks: ['dist'],
+        options: {
+          spawn: false,
+        },
+      },
     }
   });
 
   grunt.loadNpmTasks('grunt-browserify');
   grunt.loadNpmTasks('grunt-contrib-clean');
-  grunt.loadNpmTasks('grunt-contrib-connect');
+  grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-copy');
 
   grunt.registerTask('dist', ['clean:bundle','browserify', 'clean:server','copy:public']);
 
   // Default task.
-  grunt.registerTask('default', ['dist']);
+  grunt.registerTask('default', ['dist', 'watch:dist']);
 };
