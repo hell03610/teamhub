9b1cca2eff36b9b2628c155d03bc3c0965a23b80
hell03610 <eraseunavez@gmail.com>
2014-08-09 19:35:27 +0100

    Move index and app bundle to public. Remove connect server.

--- client/gruntfile.js
+++ client/gruntfile.js
@@ -10,42 +10,22 @@ module.exports = function(grunt) {
     browserify: {
       client: {
         src: ['src/**/*.js'],
-        dest: 'app.bundle.js',
+        dest: 'public/js/app.bundle.js',
         options: {
-          watch: true,
-          keepAlive: true,
           transform: ['browserify-handlebars']
         }
       }
     },
     clean:{
-      bundle: ['app.bundle.js']
-    },
-    connect: {
+      bundle: ['public/js/app.bundle.js'],
       server: {
-        options: {
-          port: 3000,
-           middleware: function(connect, options, middlewares) {
-            // inject a custom middleware into the array of default middlewares
-            middlewares.unshift(function(req, res, next) {
-
-              if (req.url.indexOf('.js') > -1 || req.url.indexOf('public/') > -1) return next();
-
-              var fs = require('fs');
-                fs.readFile('index.html', function (err, data) {
-                  if (err) {
-                    next(err);
-                    return;
-                  }
-                  res.writeHead(200, {'Content-type':'text/html'});
-                  res.write(data);
-                  res.end();
-              });
-            });
-
-            return middlewares;
-          }
-        }
+        options: {force:true},
+        src: ['../server/public']
+      }
+    },
+    copy: {
+      public: {
+        files:[{expand: true, src: ['public/**'], dest: '../server/'}],
       }
     }
   });
@@ -53,7 +33,10 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-browserify');
   grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-contrib-connect');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+
+  grunt.registerTask('dist', ['clean:bundle','browserify', 'clean:server','copy:public']);
 
   // Default task.
-  grunt.registerTask('default', ['connect:server','clean:bundle','browserify']);
+  grunt.registerTask('default', ['dist']);
 };
